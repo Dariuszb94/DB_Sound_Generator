0ada748c9802202a04ed245d1387659ccb01dcb7
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2020-05-09 20:02:23 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,4 +1,3 @@
-
 //Style for active pad
 const soundOnPad = {
   backgroundColor: 'orange' };
@@ -6,7 +5,6 @@ const soundOnPad = {
 //Style for inactive pad
 const soundOffPad = {
   backgroundColor: '#828282' };
-
 
 //creating class for object with sound
 class banking {
@@ -20,8 +18,6 @@ class banking {
 const bank = []; //declaring object for sound
 
 //declaring 9 pads
-
-
 bank[0] = new banking(81, 'Q', 'Shoot Arrow', 'http://soundbible.com/mp3/fire_bow_sound-mike-koenig.mp3');
 bank[1] = new banking(87, 'W', 'Teleport', 'https://www.myinstants.com/media/sounds/dbz-teleport.mp3');
 bank[2] = new banking(69, 'E', 'Air Wrench', 'http://soundbible.com/mp3/Air_Wrench_Short-Lightning_McQue-2139303794.mp3');
@@ -38,27 +34,29 @@ class DrumPad extends React.Component {
     super(props);
     this.state = {
       padStyle: soundOffPad };
-
     this.activateSoundEffect = this.activateSoundEffect.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
     this.activatePad = this.activatePad.bind(this);
   }
+  
   componentWillUnmount() {
     document.removeEventListener('keydown', this.handleKeyPress);
   }
+  
   componentDidMount() {
     document.addEventListener('keydown', this.handleKeyPress);
   }
 
   handleKeyPress(x) {
     if (x.keyCode === this.props.keyCode) {
-      this.activateSoundEffect();}}
+      this.activateSoundEffect();
+    }
+  }
 
   activatePad() {
     this.state.padStyle.backgroundColor === 'orange' ?
     this.setState({
       padStyle: soundOffPad }) :
-
     this.setState({
       padStyle: soundOnPad });
   }
@@ -79,22 +77,21 @@ class DrumPad extends React.Component {
         style: this.state.padStyle },
       React.createElement("audio", { className: "clip", id: this.props.keyTrigger, src: this.props.clip }),
       this.props.keyTrigger));
-
   }}
+
 //React components-nesting single pads into the body
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      display: String.fromCharCode(160) };
-
+      display: String.fromCharCode(160) 
+    };
     this.displayClipName = this.displayClipName.bind(this);
     this.clearDisplay = this.clearDisplay.bind(this);
   }
   displayClipName(name) {
     this.setState({
       display: name });
-
   }
 
   clearDisplay() {
@@ -111,23 +108,19 @@ class App extends React.Component {
           clip: padBankArr[i].url,
           keyTrigger: padBankArr[i].keyTrigger,
           keyCode: padBankArr[i].keyCode,
-          updateDisplay: this.displayClipName }));
-
-
+          updateDisplay: this.displayClipName 
+        }
+       )
+      );
     });
     return (
       React.createElement("div", { id: "drum-machine", className: "inner-container" },
       React.createElement("div", { id: "title" }, React.createElement("p", null, "Sound Generator")),
       React.createElement("div", { className: "pad-bank" },
       padBank),
-
-
       React.createElement("div", { className: "controls-container" },
       React.createElement("p", { id: "display" },
       this.state.display))));
-
-
-
   }}
 
 ReactDOM.render(
